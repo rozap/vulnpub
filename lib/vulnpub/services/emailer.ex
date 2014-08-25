896dab08f2ed6d933d7e72a5ea98cbf0143b8504
chris <chrisd1891@gmail.com>
2014-08-24 21:21:44 -0700

    emailer

--- lib/vulnpub/services/emailer.ex
+++ lib/vulnpub/services/emailer.ex
@@ -1,6 +1,11 @@
 defmodule Service.Emailer do
   use GenServer
   use Jazz
+  use HTTPotion.Base
+
+  @api "https://mandrillapp.com/api/1.0/"
+  @message_send "messages/send.json"
+  @js_headers %{"Content-Type" => "application/json"}
 
   def start_link(state, opts) do
     GenServer.start_link(__MODULE__, state, opts)
@@ -11,12 +16,32 @@ defmodule Service.Emailer do
     {:ok, state}
   end
 
-  def handle_cast({:activate, user}, state) do
-    key = "foo"
-    :io.format("CWD ~p~n", [File.cwd])
-    template = File.read! "lib/vulnpub/templates/emails/activation.json"
 
-    :io.format("EMAIL TEMPLATE ~p~n", [template])
+  defp interpolate(str, dict) do
+    Enum.reduce(dict, str, fn({key, value}, acc) -> 
+      String.replace(acc, "#\{" <> Atom.to_string(key) <> "\}", value) end)
+  end
+
+
+  defp post(url, payload) do
+    HTTPotion.start
+    :io.format("post to ~p ~n body: ~n ~p ~n", [@api <> url, payload])
+    HTTPotion.post @api <> url, payload, @js_headers
+  end
+
+  defp send(payload) do
+    post @message_send, payload
+  end
+
+  def handle_cast({:activate, user}, state) do
+    template = File.read! "lib/vulnpub/templates/emails/activation.json"
+    payload = interpolate(template, %{:key => "some key", :email => user.email, :username => user.username})
+    if HTTPotion.Response.success? (send payload) do
+      :io.format("Email sent")
+    else
+      #TODO error handle here
+      :io.format("Sending message failed! ~n Email ~p ~n payload ~p ~n", [user.email, payload])
+    end
     {:noreply, state}
   end
 
