ce025cafc22e3f87715c343964a13bfe4afa3601
Chris Duranti <chrisd1891@gmail.com>
2014-10-31 00:04:58 -0700

    ~~tests pass~~

--- web/services/manifest_parsers/unmanaged.ex
+++ web/services/manifest_parsers/unmanaged.ex
@@ -1,14 +1,14 @@
 
 
 defmodule Manifest.Parser.Unmanaged do
-  # require Manifest.Parser.Parser
-  # use Manifest.Parser.Parser
   import Manifest.Parser.Parser
 
-
-
   def parse(node, monitor) do
-    Enum.map(node, fn details -> {details["name"], details["version"]} end)
+    Enum.map(node, fn details -> {
+    	details["name"], 
+    	find_digits(details["version"]), 
+    	details["version"]
+    } end)
       |> create_packages(monitor)
   end
 
