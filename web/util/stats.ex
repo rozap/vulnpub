3ac784b55d68350a6cfbce90ca6146ca92579e5a
chris <chrisd1891@gmail.com>
2014-11-13 21:14:52 -0800

    better stats

--- web/util/stats.ex
+++ web/util/stats.ex
@@ -9,8 +9,17 @@ defmodule Plug.Stats do
     before_time = :os.timestamp()
     
     Plug.Conn.register_before_send(conn, fn conn -> 
+      params = Dict.to_list(conn.params)
+      url = conn.path_info
+        |> Enum.map(fn param ->
+          case Enum.filter(params, fn {key, value} -> value == param end) do
+            [] -> param
+            [{key, value}] -> "<#{key}>"
+          end
+        end)
+        |> Enum.join("/")
 
-      url = Enum.join(conn.path_info, "/")
+      IO.puts "URL IS #{url}"
       after_time = :os.timestamp()
       diff = :timer.now_diff(after_time, before_time) / 1000
       GenServer.cast(:stats_collector, {:insert, "latency", [value: diff, url: url]})
